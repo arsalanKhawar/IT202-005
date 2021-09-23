0bda76d16f9caf482ecada14118e6681ae5ead9a
arsalanKhawar <ak78@njit.edu>
2021-09-23 00:21:11 -0400

    final

--- public_html/hw2/problem1.php
+++ public_html/hw2/problem1.php
@@ -8,7 +8,8 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
-        for($i = 0; $i < count($arr); $i++){
+        for($i = 0; $i < count($arr); $i++)
+        {
             if($arr[$i] % 2 != 0){
                 echo($arr[$i] . " ");
             }
