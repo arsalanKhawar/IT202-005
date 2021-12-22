61158fd47a2cf39a5952a849aa3fc290bb799508
arsalanKhawar <ak78@njit.edu>
2021-12-21 21:41:13 -0500

    can add items to orders and orderitems tables

--- public_html/Project/cart.php
+++ public_html/Project/cart.php
@@ -21,6 +21,50 @@ try {
     flash("<pre>" . var_export($e, true) . "</pre>");
 }
 ?>
+
+<html>
+<style>
+input[type=text], select {
+  width: 20%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=number], select {
+  width: 20%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+
+</style>
+<body>
+
+
 <div class="container-fluid">
     <h1>Your Cart</h1>
 
@@ -128,7 +172,126 @@ try {
 <br>
 <h2>Your total is: <?php se($total) ?></h2>
 
+
+
         
 <?php
+$result = [];
+$columns = get_columns("User_cart");
+$ignore = ["id", "modified", "created"];
+$db = getDB();
+//get the item
+$id = se($_GET, "id", -1, false);
+$stmt = $db->prepare("SELECT * FROM User_cart where id =:id");
+try {
+    $stmt->execute([":id" => $id]);
+    $r = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($r) {
+        $result = $r;
+    }
+} catch (PDOException $e) {
+    flash("<pre>" . var_export($e, true) . "</pre>");
+}
+
+?>
+
+
+<div class="container-fluid">
+    <h1>Checkout</h1>
+        <form method="POST">
+            <label for="payment_method">Payment Method:</label>
+            <input type="text" name="payment_method">
+            <label for="cart_total">cart total:</label>
+            <input type="number" name="cart_total">
+            <label for="Name">Name:</label>
+            <input type="text" name="Name">
+            <label for="address">Address:</label>
+            <input type="text" name="address">
+            <label for="city">City:</label>
+            <input type="text" name="city">
+            <label for="state">State:</label>
+            <input type="text" name="state">
+            <label for="zipcode">Zipcode:</label>
+            <input type="text" name="zipcode">
+            <input type="hidden" name="user_id" value="<?php se(get_user_id(), 'user_id'); ?>" />
+            <input type="hidden" name="total_price" value="<?php se($total, 'total_price'); ?>" />
+            <input type="submit" value="Purchase">
+        </form>
+</div>
+
+<?php
+if(se($_POST, "total_price" , "", false) != se($_POST, "cart_total" , "", false)) {
+flash("invalid cart total");
+}
+else if (isset($_POST["payment_method"]) && isset($_POST["user_id"]) && isset($_POST["cart_total"])) {
+    $address = se($_POST, "address", "", false) . ", " . se($_POST, "city", "", false) . ", " . se($_POST, "state", "", false) . " " . se($_POST, "zipcode", "", false);
+    $user_id = se($_POST, "user_id", "", false);
+    $total_price = se($_POST, "total_price", "", false);
+    $payment_method = se($_POST, "payment_method", "", false);
+
+    $db = getDB();
+    $stmt = $db->prepare("INSERT INTO Orders (user_id, total_price, address, payment_method) VALUES( :user_id, :total_price, :address, :payment_method)");
+    try {
+        $stmt->execute([":address" => $address, ":user_id" => $user_id, ":total_price" => $total_price, ":payment_method" => $payment_method]);
+        flash("purchased!");
+    } catch (Exception $e) {
+        if(is_logged_in()){
+        flash("There was a problem");
+        }
+        else{
+            flash("You need to be logged in to purchase.");
+        }
+        
+    }
+    
+}
+
+?>
+
+<?php 
+
+//add items into OrderItems
+
+if (isset($_POST["payment_method"]) && isset($_POST["user_id"]) && isset($_POST["cart_total"])) {
+
+    $results = [];
+    $columns = get_columns("User_cart");
+    $ignore = ["id", "modified", "created"];
+    $db = getDB();
+    $userid = get_user_id();
+    //get the item
+    $stmt = $db->prepare("SELECT * FROM User_cart WHERE user_id =:user_id");
+    try {
+        $stmt->execute([":user_id" => $userid]);
+        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($r) {
+            $results = $r;
+        }
+    } catch (PDOException $e) {
+        flash("<pre>" . var_export($e, true) . "</pre>");
+    }
+foreach ($results as $index => $record){
+               
+
+                    foreach ($record as $column => $value){ 
+                        if (!in_array($column, $ignore)){
+                            
+                                $stmt = $db->prepare("INSERT INTO OrderItems (product_id, order_id, unit_price, quntity) VALUES(:product_id, :order_id, :unit_cost, :quntity)");
+                                try {
+                                    $stmt->execute([":product_id" => $results[$index]["product_id"], ":order_id" => $results[$index]["user_id"], ":unit_cost" => $results[$index]["unit_cost"], ":quntity" => $results[$index]["desired_quantity"]]);
+                                } catch (Exception $e) {
+                                    if(is_logged_in()){
+                                    flash("There was a problem");
+                                    }
+                                    else{
+                                        flash("You need to be logged in to purchase.");
+                                    }
+                                    
+                                }
+                            }
+                    }
+  }
+}
+
 require(__DIR__ . "/../../partials/flash.php");
 ?>
